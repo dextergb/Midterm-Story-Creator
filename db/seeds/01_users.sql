0e6c3968a2e49c40731270238e42e252fd65a378
Andyiev <ayevdokymenko@gmail.com>
2021-06-26 17:55:39 +0000

    Change seeds for users db

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (full_name, email, nick_name, password) VALUES ('Alice','yahoo@yahoo.com','Coolwriter','password');
-INSERT INTO users (full_name, email, nick_name, password) VALUES ('Kira','gmail@gmail.com','Coolwriter','password');
+INSERT INTO users (full_name, email, nick_name, password) VALUES ('Alice','yahoo@yahoo.com','Coolwriter');
+INSERT INTO users (full_name, email, nick_name, password) VALUES ('Kira','gmail@gmail.com','Verycoolwriter');
