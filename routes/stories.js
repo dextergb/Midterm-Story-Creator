3df7779e114fed0103b92ec71cc69562bd4ba008
Andyiev <ayevdokymenko@gmail.com>
2021-06-28 23:45:23 +0000

    some more code for stories

--- routes/stories.js
+++ routes/stories.js
@@ -23,42 +23,44 @@ module.exports = () => {
   });
 
   // GET for contribute
-  router.get("/contribute", (req, res) => {
-    const storyId = req.params.storyID;
-    const storyBody = req.params.story_body;
-    //console.log(storyId);
-    //console.log(query);
-
-
-        res.render(stories_collab);
-
+  router.get("/:storyID/contribute", (req, res) => {
+    //res.send("Got it");
+    res.render("stories_collab.ejs")
   });
-   // router.post("/:storyID", (req,res) => {
-  //   db.query(`INSERT INTO contributed_stories
-  //   (story_id, user_id, contributed_body, contributed_vote, accepted_contribution)
-  //   VALUES
-  //   ($1, $2, $3, $4, $5) RETURNING*`
-  //   , [story_id, user_id, contributed_body, contributed_vote, accepted_contribution])
-  //   .then((response) => {
-  //     res.json(response.rows);
-  //   })
-  //   .catch((err) => {
-  //     res.status(500).json({ error: err.message });
-  //   });
-  // })
+  // POST for contribute
+  router.post("/:storyID/contribute", (req,res) => {
+    db.query(`INSERT INTO contributed_stories
+    (story_id, user_id, contributed_body, contributed_vote, accepted_contribution)
+    VALUES
+    ($1, $2, $3, $4, $5) RETURNING*`
+    , [story_id, user_id, contributed_body, contributed_vote, accepted_contribution])
+    .then((response) => {
+      res.json(response.rows);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+  })
 
-  router.get("/", (req, res) => {
-    console.log("This is for all stories");
-    db.query(`SELECT stories.* FROM stories`)
-      .then((data) => {
-        console.log(data);
-        // needs to figure out what we need here
-        const stories = data.rows[0]["story_body"];
-        res.json({ stories });
-        //res.render("stories")
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
+  // should be for user, not for all
+  router.get("/:", (req, res) => {
+    const userId = req.session["user_id"];
+
+      if (!userId) {
+        // if user is not logged , he will be redirected to the main page again
+      return res.redirect('/login');
+      }
+      db.query(`SELECT stories.*, users.nick_name
+      FROM stories
+      JOIN users ON stories.user_id = users.id
+      WHERE stories.user_id = $1`, [userId])
+        .then((data) => {
+          console.log(data.rows[0]);
+          //const stories = data.rows[0]["story_body"];
+          res.render("stories.ejs");
+        })
+        .catch((err) => {
+          res.status(500).json({ error: err.message });
       });
   });
   return router;
