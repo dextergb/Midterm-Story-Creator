68d2556ae2694763a7968c4f3ee59c672c7fa6e9
Sam Roy-Effer <samroyeffer@gmail.com>
2021-06-28 14:53:40 -0400

    restructure login page

--- routes/login.js
+++ routes/login.js
@@ -2,13 +2,32 @@ const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 const db = require("../db/database");
+const cookieSession = require("cookie-session");
+const { Template } = require("ejs");
+const authenticationOfUsers = require("../routes/helper_functions/helper_functions");
 
 module.exports = () => {
   router.get("/login", (req, res) => {
-    const userCookies = db.query(
-      `SELECT users.nick_name users.email FROM users
-    WHERE users.email = $1 AND users.nick_name = = $2`,
-      []
-    );
+    const userCookie = req.session["user_id"];
+    if (userCookie) {
+      res.redirect("/");
+    } else {
+      const templateVars = {
+        user: null,
+      };
+      res.render("loginpage", templateVars);
+    }
   });
+  router.post("/login", (req, res) => {
+    const email = req.body.email;
+    const user = req.body.user;
+    const username = authenticationOfUsers(user, email, db);
+    if (username) {
+      req.session["user_id"] = user.id;
+      res.redirect("/");
+    } else {
+      res.status(401).send("Please Try Again.");
+    }
+  });
+  return router;
 };
--- server.js
+++ server.js
@@ -41,13 +41,12 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
-const mainPage = require("./routes/main-page");
-
+const loginRoute = require("./routes/login");
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/users", usersRoutes());
 app.use("/widgets", widgetsRoutes());
-app.use("", mainPage());
+app.use("/login", loginRoute());
 // Note: mount other resources here, using the same pattern above
 
 // Home page
