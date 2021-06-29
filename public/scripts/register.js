31d7c61003c063485f1e8da075d34b50ce9ad915
dextergb <dextergb@rogers.com>
2021-06-29 22:00:08 +0000

    redesign login and register

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -1,32 +1,21 @@
-$(function () {
-  $(".btn").click(function () {
-    $(".form-signin").toggleClass("form-signin-left");
-    $(".form-signup").toggleClass("form-signup-left");
-    $(".frame").toggleClass("frame-long");
-    $(".signup-inactive").toggleClass("signup-active");
-    $(".signin-active").toggleClass("signin-inactive");
-    $(".forgot").toggleClass("forgot-left");
-    $(this).removeClass("idle").addClass("active");
-  });
+function field_focus(field, email) {
+  if (field.value == email) {
+    field.value = "";
+  }
+}
+
+function field_blur(field, email) {
+  if (field.value == "") {
+    field.value = email;
+  }
+}
+
+//Fade in dashboard box
+$(document).ready(function () {
+  $(".box").hide().fadeIn(1000);
 });
 
-$(function () {
-  $(".btn-signup").click(function () {
-    $(".nav").toggleClass("nav-up");
-    $(".form-signup-left").toggleClass("form-signup-down");
-    $(".success").toggleClass("success-left");
-    $(".frame").toggleClass("frame-short");
-  });
-});
-
-$(function () {
-  $(".btn-signin").click(function () {
-    $(".btn-animate").toggleClass("btn-animate-grow");
-    $(".welcome").toggleClass("welcome-left");
-    $(".cover-photo").toggleClass("cover-photo-down");
-    $(".frame").toggleClass("frame-short");
-    $(".profile-photo").toggleClass("profile-photo-down");
-    $(".btn-goback").toggleClass("btn-goback-up");
-    $(".forgot").toggleClass("forgot-fade");
-  });
+//Stop click event
+$("a").click(function (event) {
+  event.preventDefault();
 });
