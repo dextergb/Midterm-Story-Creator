d9c8ecc2e676f1839db179e5e419d9d65dd1e4f8
Sam Roy-Effer <samroyeffer@gmail.com>
2021-06-29 13:10:00 -0400

    add new story

--- routes/new-story.js
+++ routes/new-story.js
@@ -3,7 +3,24 @@ const router = express.Router();
 const db = require("../db/database");
 module.exports = () => {
   router.get("/", (req, res) => {
-    res.render("index");
+    const userId = req.session["user_id"];
+    const userEmail = req.session.email;
+
+    if (!userId) {
+      // if user is not logged , he will be redirected to the main page again
+      return res.redirect("/login");
+    }
+    if (authenticationOfUsers(userId, userEmail, db) === true) {
+      return res.render("stories_new.ejs");
+    }
+  });
+
+  router.post("/", (req, res) => {
+    const newStoryBody = req.params.body;
+
+    db.query(
+      `INSET INTO stories (user_id, story_body, votes, completed, user_name) VALUES ($1,$2,$3,$4,$5)`
+    );
   });
 
   return router;
--- routes/stories.js
+++ routes/stories.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../db/database");
+const authenticationOfUsers = require("./helper_functions/helper_functions");
 
 module.exports = () => {
   router.get("/:storyID", (req, res) => {
@@ -8,10 +9,13 @@ module.exports = () => {
     const storyBody = req.params.story_body;
     //console.log(storyId);
     //console.log(query);
-    db.query(`SELECT stories.*, users.nick_name
+    db.query(
+      `SELECT stories.*, users.nick_name
     FROM stories
     JOIN users ON stories.user_id = users.id
-    WHERE stories.id = $1`, [storyId])
+    WHERE stories.id = $1`,
+      [storyId]
+    )
       .then((data) => {
         console.log(data.rows[0]);
         const stories = data.rows[0]["story_body"];
@@ -25,46 +29,59 @@ module.exports = () => {
   // GET for contribute
   router.get("/:storyID/contribute", (req, res) => {
     //res.send("Got it");
-    res.render("stories_collab.ejs")
+    res.render("stories_collab.ejs");
   });
   // POST for contribute
-  router.post("/:storyID/contribute", (req,res) => {
-    db.query(`INSERT INTO contributed_stories
+  router.post("/:storyID/contribute", (req, res) => {
+    db.query(
+      `INSERT INTO contributed_stories
     (story_id, user_id, contributed_body, contributed_vote, accepted_contribution)
     VALUES
-    ($1, $2, $3, $4, $5) RETURNING*`
-    , [story_id, user_id, contributed_body, contributed_vote, accepted_contribution])
-    .then((response) => {
-      res.json(response.rows);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-    });
-  })
+    ($1, $2, $3, $4, $5) RETURNING*`,
+      [
+        story_id,
+        user_id,
+        contributed_body,
+        contributed_vote,
+        accepted_contribution,
+      ]
+    )
+      .then((response) => {
+        res.json(response.rows);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
 
   // should be for user, not for all
   router.get("/:storyID/complete", (req, res) => {
     const userId = req.session["user_id"];
 
-      if (!userId) {
-        // if user is not logged , he will be redirected to the main page again
-      return res.redirect('/login');
-      }
-      res.render("stories.ejs");
+    if (!userId) {
+      // if user is not logged , he will be redirected to the main page again
+      return res.redirect("/login");
+    }
+    res.render("stories.ejs");
   });
 
   // To get story completed and redirect to the main page
   router.post("/:storyID/complete", (req, res) => {
     const userId = req.session["user_id"];
+    const userEmail = req.session.email;
     const storyId = req.session.story_id;
 
-      if (!userId) {
-        // if user is not logged , he will be redirected to the main page again
-      return res.redirect('/login');
-      }
-      db.query(`UPDATE stories
+    if (!userId) {
+      // if user is not logged , he will be redirected to the main page again
+      return res.redirect("/login");
+    }
+    if (authenticationOfUsers(userId, userEmail, db) === true) {
+      db.query(
+        `UPDATE stories
       SET completed = true
-      WHERE story_id = $1;`, [storyId])
+      WHERE story_id = $1;`,
+        [storyId]
+      )
         .then((data) => {
           console.log(data.rows[0]);
           //const stories = data.rows[0]["story_body"];
@@ -72,8 +89,10 @@ module.exports = () => {
         })
         .catch((err) => {
           res.status(500).json({ error: err.message });
-      });
+        });
+    }
   });
+
   return router;
 };
 
