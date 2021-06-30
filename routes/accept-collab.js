5b6145fd349ff814a5c141b27e0d71fa5396c078
Sam Roy-Effer <samroyeffer@gmail.com>
2021-06-30 15:55:23 -0400

    start of accept collab

---
+++ routes/accept-collab.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db/database");
+const authenticationOfUsers = require("./helper_functions/helper_functions");
+const { response } = require("express");
+
+module.exports = () => {};
