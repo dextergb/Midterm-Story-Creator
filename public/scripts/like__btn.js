5769deebc02bf85d62f6cf913107488a08e68b62
dextergb <dextergb@rogers.com>
2021-06-30 15:50:47 +0000

    refactor

--- public/scripts/like__btn.js
+++ public/scripts/like__btn.js
@@ -1,18 +1,20 @@
 $(document).ready(function () {
-  $('.btn-counter').click(function (event) {
+  $(".btn-counter").click(function (event) {
     // Don't follow the link
     event.preventDefault();
-    console.log(`cliecked incerement`)
+    console.log(`clicked incerement`);
     // take a story id. Just for now - static
     //let storyId = req.params.storyID;
-    let storyId = 1
+    let storyId = 1;
     $.ajax({
       method: "POST",
       url: `/stories/${storyId}/increment`,
-    }).done((res) => {
-      console.log(`succes updated story: ${res}`)
-    }).fail((err)=>{
-      console.log(`failed update with err: ${err}`)
     })
+      .done((res) => {
+        console.log(`success updated story: ${res}`);
+      })
+      .fail((err) => {
+        console.log(`failed update with err: ${err}`);
+      });
   });
 });
