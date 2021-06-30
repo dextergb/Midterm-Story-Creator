06a5c19e193713da317d21c33360efbdb0a277e4
Sam Roy-Effer <samroyeffer@gmail.com>
2021-06-30 12:08:31 -0400

    fix spacing in file

--- public/scripts/like__btn.js
+++ public/scripts/like__btn.js
@@ -1,14 +1,11 @@
 $(document).ready(function () {
   $(".btn-counter").on("click", function (event, count) {
     event.preventDefault();
-
     var $this = $(this),
       count = $this.attr("data-count"),
       active = $this.hasClass("active"),
       multiple = $this.hasClass("multiple-count");
-
     $.fn.noop = $.noop;
-
     const story_id = event.target.id;
     $(() => {
       $.ajax({
