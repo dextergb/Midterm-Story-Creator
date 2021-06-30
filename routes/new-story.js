3071f272f291fb860d9055eda963e0adb949d31f
Sam Roy-Effer <samroyeffer@gmail.com>
2021-06-30 13:19:12 -0400

    fix userID not registering on other pages

--- routes/new-story.js
+++ routes/new-story.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../db/database");
+const { response } = require("express");
 module.exports = () => {
   router.get("/", (req, res) => {
     const userId = req.session["user_id"];
     const userEmail = req.session.email;
+    const templateVars = {
+      stories: response.rows,
+      userID: req.session.user_id,
+    };
 
     if (!userId) {
       // if user is not logged , he will be redirected to the main page again
       return res.redirect("/login");
     }
     if (authenticationOfUsers(userEmail, db) === true) {
-      return res.render("stories_new.ejs");
+      return res.render("stories_new.ejs", templateVars);
     }
   });
 
