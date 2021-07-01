6d5a6771e20763b0a2607ecff4018aec4c5824f2
Sam Roy-Effer <samroyeffer@gmail.com>
2021-07-01 15:48:56 -0400

    adjust vote button query

--- routes/vote_button.js
+++ routes/vote_button.js
@@ -6,22 +6,20 @@ module.exports = () => {
   router.post("/", (req, res) => {
     const story_id = req.body.param;
     console.log(story_id);
-    db.query("SELECT votes FROM stories WHERE id = $1", [story_id]).then(
-      (data) => {
-        const voteCount = data.rows[0].votes + 1;
-        db.query("UPDATE stories SET votes =  $1 WHERE id = $2", [
-          voteCount,
-          story_id,
-        ])
-          .then((data) => {
-            console.log(voteCount);
-            return res.status(200).send({ message: voteCount });
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
-    );
+
+    db.query(
+      "UPDATE stories SET votes = votes + 1 WHERE id = $1 RETURNING votes",
+      [story_id]
+    )
+      .then((data) => {
+        const voteCount = data.rows[0].votes;
+        console.log(voteCount);
+        return res.status(200).send({ message: voteCount });
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+        console.log(err);
+      });
   });
   return router;
 };
