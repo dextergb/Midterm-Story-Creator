803298439bda8a12df3fea6f742771ff464b97ad
Sam Roy-Effer <samroyeffer@gmail.com>
2021-07-01 18:29:54 -0400

    fix new story character counter

--- public/scripts/character_count_collab.js
+++ public/scripts/character_count_collab.js
@@ -11,4 +11,17 @@ $(document).ready(function () {
       );
     }
   });
+
+  $("#story-text").on("input", function () {
+    let counter = 500 - this.value.length;
+    if (counter >= 0) {
+      $(".counter").replaceWith(
+        `<output name="counter" class="counter" style="color: black" for="tweet-text">${counter}</output>`
+      );
+    } else {
+      $(".counter").replaceWith(
+        `<output name="counter" class="counter" style="color: red" for="tweet-text">${counter}</output>`
+      );
+    }
+  });
 });
